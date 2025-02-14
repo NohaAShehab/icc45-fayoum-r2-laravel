40dc3091329b692d3da560421e1ad5f2f614d002
Noha Shehab <nohashehab.iti@gmail.com>
2025-02-14 11:40:58 +0200

    lab03

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -10,11 +10,15 @@ class Product extends Model
     // you will use factories
     use HasFactory;
 
+    // model have all properties related to the object
     // if you want product model --> represent table allproducts
     protected $table = 'products';
 
     # model --> define relations inside model ??
     # product has one category ??
+    # any product from this model --> have one to many relationship
+    # with category model
+    # define relation
     function category(){
         # this product belongs to one category
         # select * from categories where id = product.category_id;
